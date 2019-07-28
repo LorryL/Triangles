f803cbf4a2c4f4fdb5c25535f0aedda2b0ce8b64
Laurie Mills <lauriemills44@hotmail.com>
2019-07-28 17:26:40 +0100

    Logic for calculating (V1x, V1y)

--- Triangles/Providers/CoordinatesProvider.cs
+++ Triangles/Providers/CoordinatesProvider.cs
@@ -8,15 +8,64 @@ namespace Triangles.Providers
     public class CoordinatesProvider
     {
 
-
+        /*
+         * Some assumptions made.....
+         * 
+         * Top left corner is point 0,0 with x axis moving left and y axis moving down.
+         * 
+         * (V1x,V1y) is the right angled corner
+         * (V2x,V2y) is top corner
+         * (V3x,V3y) is bottom corner
+         * 
+         */
         public static string CalculateCoordinates(string row, int column)
         {
+            int rowNumber = ConvertRowLetterToNumber(row);
             if (column % 2 == 0)
-            { return "even column"; }
-            return "odd column"; ;
+            { return CalculateV1ForEvenRow(rowNumber, column); }
+            return CalculateV1ForOddRow(rowNumber, column);
         }
 
-       
+        private static int ConvertRowLetterToNumber(string row)
+        {
+            switch (row)
+            {
+                case "a":
+                    return 1;
+                case "b":
+                    return 2;
+                case "c":
+                    return 3;
+                case "d":
+                    return 4;
+                case "e":
+                    return 5;
+                case "f":
+                    return 6;
+                default:
+                    return 0;
+            }
+        }
+
+        private static string CalculateV1ForOddRow(int row, int column)
+        {
+            int v1x = ((column +1 ) * 10) / 2-10;
+            int v1y = row * 10;
+
+            return string.Format("V1({0}, {1})", v1x, v1y);
+
+        }
+
+        private static string CalculateV1ForEvenRow(int row, int column)
+        {
+            int v1x = (column * 10) / 2;
+            int v1y = row * 10 - 10;
+
+            return string.Format("V1({0}, {1})", v1x, v1y);
+
+        }
+
+
     }
 
 }
